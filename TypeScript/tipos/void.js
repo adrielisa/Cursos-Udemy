6c0a14c826093d22bdd1129d039f22cabf337563
adrielisa <danielganer67@gmail.com>
2025-08-14 15:36:26 -0500

    Void

---
+++ TypeScript/tipos/void.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    //Significa que no regresaré nada
+    function callBatman() {
+        return;
+    }
+    const callSuperman = () => {
+    };
+    const a = callBatman();
+    console.log(a);
+})();
---
+++ TypeScript/tipos/void.ts
@@ -0,0 +1,15 @@
+(()=>{
+
+    //Significa que no regresaré nada
+    function callBatman():void {
+        return
+    }
+
+    const callSuperman = ():void =>{
+        
+    }
+
+    const a =  callBatman()
+    console.log(a)
+
+})();
