833155108ed886b2e3376b334e15203dcf923e39
adrielisa <danielganer67@gmail.com>
2025-08-14 14:36:17 -0500

    Strings

---
+++ TypeScript/tipos/strings.js
@@ -0,0 +1,7 @@
+"use strict";
+(() => {
+    const batman = 'Batman';
+    const linternaVerde = "Linterna's verde";
+    const volcanNegro = "Héroe: Volcan Negro";
+    console.log(`I'm ${batman}`);
+})();
---
+++ TypeScript/tipos/strings.ts
@@ -0,0 +1,8 @@
+(()=>{
+    const batman = 'Batman';
+    const linternaVerde = "Linterna's verde"
+    const volcanNegro = "Héroe: Volcan Negro"
+
+    console.log(`I'm ${ batman}, `)
+})();
+
