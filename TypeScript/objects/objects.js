5f26c3b71f76e27fa07c3c6c5def1ee46e22db6b
adrielisa <danielganer67@gmail.com>
2025-08-15 10:54:01 -0500

    Objetos

---
+++ TypeScript/objects/objects.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Alen',
+        age: 24,
+        powers: ['Súper velocidad', 'Viajar en el tiempo']
+    };
+    flash = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Súper fuerza']
+    };
+})();
---
+++ TypeScript/objects/objects.ts
@@ -0,0 +1,14 @@
+(()=>{
+
+    let flash = {
+        name: 'Barry Alen',
+        age: 24,
+        powers: ['Súper velocidad', 'Viajar en el tiempo']
+    }
+
+    flash = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Súper fuerza']
+    }
+})();
