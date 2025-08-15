bce150c850b21092cf6d2d2b86c3230870f58197
adrielisa <danielganer67@gmail.com>
2025-08-15 11:14:33 -0500

    Objetos

--- TypeScript/objects/objects.js
+++ TypeScript/objects/objects.js
@@ -1,5 +1,8 @@
 "use strict";
 (() => {
+    var _a;
+    //Al crear objetos podemos especificar que tipo de datos tendrá
+    //Para el último dato decimos que es una función opcional de tipo string
     let flash = {
         name: 'Barry Alen',
         age: 24,
@@ -7,7 +10,11 @@
     };
     flash = {
         name: 'Clark Kent',
-        age: 60,
-        powers: ['Súper fuerza']
+        //age: 60,
+        powers: ['Súper fuerza'],
+        getName() {
+            return this.name;
+        }
     };
+    console.log((_a = flash.getName) === null || _a === void 0 ? void 0 : _a.call(flash));
 })();
--- TypeScript/objects/objects.ts
+++ TypeScript/objects/objects.ts
@@ -1,6 +1,8 @@
 (()=>{
 
-    let flash = {
+    //Al crear objetos podemos especificar que tipo de datos tendrá
+    //Para el último dato decimos que es una función opcional de tipo string
+    let flash: { name: string, age?: number, powers: string[], getName?: () => string} = {
         name: 'Barry Alen',
         age: 24,
         powers: ['Súper velocidad', 'Viajar en el tiempo']
@@ -8,7 +10,12 @@
 
     flash = {
         name: 'Clark Kent',
-        age: 60,
-        powers: ['Súper fuerza']
+        //age: 60,
+        powers: ['Súper fuerza'],
+        getName(){
+            return this.name;
+        }
     }
+
+    console.log(flash.getName?.())
 })();
