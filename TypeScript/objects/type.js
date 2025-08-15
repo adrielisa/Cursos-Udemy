5dcf002d65be391512a87dfba9a203a494264274
adrielisa <danielganer67@gmail.com>
2025-08-15 11:31:05 -0500

    Tipos

---
+++ TypeScript/objects/type.js
@@ -0,0 +1,18 @@
+"use strict";
+(() => {
+    var _a;
+    let flash = {
+        name: 'Barry Alen',
+        age: 24,
+        powers: ['Súper velocidad', 'Viajar en el tiempo']
+    };
+    let superman = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Super velocidad'],
+        getName() {
+            return this.name;
+        }
+    };
+    console.log((_a = flash.getName) === null || _a === void 0 ? void 0 : _a.call(flash));
+})();
